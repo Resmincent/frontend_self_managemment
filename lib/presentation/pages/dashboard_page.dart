b4ad9390b520fd9a766b3c76179ec2b8b9f7bde0
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-02-17 14:48:00 +0700

    add fragment dan bottom navigation bar

--- lib/presentation/pages/dashboard_page.dart
+++ lib/presentation/pages/dashboard_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:self_management/common/app_color.dart';
+import 'package:self_management/presentation/pages/fragments/analytic_fragment.dart';
+import 'package:self_management/presentation/pages/fragments/expense_fragment.dart';
+import 'package:self_management/presentation/pages/fragments/home_fragment.dart';
+import 'package:self_management/presentation/pages/fragments/solution_fragment.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -10,12 +16,106 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  final navIndex = 0.obs;
+
+  final List<Map<String, dynamic>> menu = [
+    {
+      'iconType': 'asset',
+      'icon': 'assets/images/home.png',
+      'view': const HomeFragment(),
+    },
+    {
+      'iconType': 'asset',
+      'icon': 'assets/images/analytic.png',
+      'view': const AnalyticFragment(),
+    },
+    {
+      'iconType': 'asset',
+      'icon': 'assets/images/agenda.png',
+      'view': const SolutionFragment(),
+    },
+    {
+      'iconType': 'material',
+      'icon': Icons.add_card,
+      'view': const ExpenseFragment(),
+    }
+  ];
+
+  Widget _buildIcon(Map<String, dynamic> menuItem, bool isActive) {
+    if (menuItem['iconType'] == 'asset') {
+      return ImageIcon(
+        AssetImage(menuItem['icon']),
+        color: isActive ? Colors.white : AppColor.primary,
+      );
+    } else {
+      return Icon(
+        menuItem['icon'],
+        color: isActive ? Colors.white : AppColor.primary,
+      );
+    }
+  }
+
+  Widget _buildMenu() {
+    return Obx(() {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: AppColor.primary.withOpacity(0.4),
+                blurRadius: 10,
+                offset: const Offset(4, 4),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(
+              menu.length,
+              (index) {
+                bool isActive = index == navIndex.value;
+                return Material(
+                  color: isActive ? AppColor.primary : Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(12),
+                    onTap: () => navIndex.value = index,
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: isActive ? AppColor.primary : Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      alignment: Alignment.center,
+                      child: _buildIcon(menu[index], isActive),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Dashboard Page'),
+      resizeToAvoidBottomInset: false,
+      body: Obx(
+        () {
+          return menu[navIndex.value]['view'];
+        },
       ),
+      extendBody: true,
+      bottomNavigationBar: _buildMenu(),
     );
   }
 }
